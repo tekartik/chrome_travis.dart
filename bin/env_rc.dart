42c07e9c50075c91f51d07559f60195bc7843a33
Alexandre Roux Tekartik <alex@tekartik.com>
2016-10-07 00:06:11 +0200

    fix: show env

--- bin/env_rc.dart
+++ bin/env_rc.dart
@@ -1,13 +1,26 @@
 import 'dart:io';
+
+import 'package:args/args.dart';
 import 'package:path/path.dart';
 
 const String envRc = "env.rc";
 
-main() async {
+main(List<String> args) async {
   Directory tempDir = await Directory.systemTemp.createTemp();
 
-  File dst = new File(join(tempDir.path, envRc));
-  await dst.writeAsString(r"""
+  ArgParser parser = new ArgParser(allowTrailingOptions: true);
+  parser.addFlag('help', abbr: 'h', help: 'Usage help', negatable: false);
+  parser.addFlag('verbose',
+      abbr: 'v', help: 'Display content in stderr', negatable: false);
+
+  ArgResults results = parser.parse(args);
+  if (results['help']) {
+    stderr.writeln(parser.usage);
+    return;
+  }
+  bool verbose = results['verbose'];
+
+  String content = r"""
 export CHROME_BIN=/usr/bin/google-chrome
 export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
@@ -16,7 +29,12 @@ sudo apt-get install -y libappindicator1 fonts-liberation
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome*.deb
 t=0; until (xdpyinfo -display :99 &> /dev/null || test $t -gt 10); do sleep 1; let t=$t+1; done
-""");
+""";
+  if (verbose) {
+    stderr.writeln(content);
+  }
+
+  File dst = new File(join(tempDir.path, envRc));
+  await dst.writeAsString(content);
   stdout.write(dst.path);
 }
-
--- bin/show_env.dart
+++ bin/show_env.dart
@@ -1,17 +1,17 @@
-import 'dart:io';
-import 'package:process_run/cmd_run.dart';
 import 'dart:convert';
+import 'dart:io';
+
+import 'package:process_run/cmd_run.dart';
 
 main() async {
-  await run('which', ['dart'] ,verbose: true);
+  await run('which', ['dart'], verbose: true);
   await run('which', ['pub'], verbose: true);
-  await run('which', ['dart2js'],verbose: true);
-  await run('which', ['google-chrome'],verbose: true);
-  await run('which', ['firefox'],verbose: true);
+  await run('which', ['dart2js'], verbose: true);
+  await run('which', ['google-chrome'], verbose: true);
+  await run('which', ['firefox'], verbose: true);
   Map info = {};
   info['Platform.operatingSystem'] = Platform.operatingSystem;
   info['Directory.current'] = Directory.current.path;
   info['Platform.environment'] = Platform.environment;
   print(const JsonEncoder.withIndent('  ').convert(info));
 }
-
