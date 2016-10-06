ea8a8cb35509f4aa55e2c36a7c1ffe53c985f8af
Alexandre Roux Tekartik <alex@tekartik.com>
2016-10-06 17:45:22 +0200

    fix: use a string content

--- bin/env_rc.dart
+++ bin/env_rc.dart
@@ -6,9 +6,17 @@ const String envRc = "env.rc";
 main() async {
   Directory tempDir = await Directory.systemTemp.createTemp();
 
-  File src = new File(join(dirname(Platform.script.toFilePath()), envRc));
   File dst = new File(join(tempDir.path, envRc));
-  await dst.writeAsString(await src.readAsString());
+  await dst.writeAsString(r"""
+export CHROME_BIN=/usr/bin/google-chrome
+export DISPLAY=:99.0
+sh -e /etc/init.d/xvfb start
+sudo apt-get update
+sudo apt-get install -y libappindicator1 fonts-liberation
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome*.deb
+t=0; until (xdpyinfo -display :99 &> /dev/null || test $t -gt 10); do sleep 1; let t=$t+1; done
+""");
   stdout.write(dst.path);
 }
 
