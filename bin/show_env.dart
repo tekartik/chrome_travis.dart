ad8e24ee312b713dd3504f1c6556284feb50bbe6
alex <alex@tekartik.com>
2019-12-10 08:40:17 +0100

    fix: travis

--- bin/show_env.dart
+++ bin/show_env.dart
@@ -3,13 +3,13 @@ import 'dart:io';
 
 import 'package:process_run/cmd_run.dart';
 
-main() async {
+Future main() async {
   await run('which', ['dart'], verbose: true);
   await run('which', ['pub'], verbose: true);
   await run('which', ['dart2js'], verbose: true);
   await run('which', ['google-chrome'], verbose: true);
   await run('which', ['firefox'], verbose: true);
-  Map info = {};
+  final info = {};
   info['Platform.operatingSystem'] = Platform.operatingSystem;
   info['Directory.current'] = Directory.current.path;
   info['Platform.environment'] = Platform.environment;
