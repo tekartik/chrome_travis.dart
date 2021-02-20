1f8a1113828fd446de8c628af428304ba76827e3
alex <alex@tekartik.com>
2021-02-20 18:20:15 +0100

    test: add github actions

--- bin/show_env.dart
+++ bin/show_env.dart
@@ -1,17 +1,20 @@
 import 'dart:convert';
 import 'dart:io';
 
-import 'package:process_run/cmd_run.dart';
+import 'package:process_run/shell.dart';
 
 Future main() async {
-  await run('which', ['dart'], verbose: true);
-  await run('which', ['pub'], verbose: true);
-  await run('which', ['dart2js'], verbose: true);
-  await run('which', ['google-chrome'], verbose: true);
-  await run('which', ['firefox'], verbose: true);
+  await run('which dart');
+  await run('which pub');
+  await run('which dart2js');
+  await run('which google-chrome');
+  await run('which firefox');
   final info = {};
   info['Platform.operatingSystem'] = Platform.operatingSystem;
   info['Directory.current'] = Directory.current.path;
   info['Platform.environment'] = Platform.environment;
+  info['vars'] = Map.from(ShellEnvironment().vars);
+  info['paths'] = List.from(ShellEnvironment().paths);
+  info['aliases'] = Map.from(ShellEnvironment().aliases);
   print(const JsonEncoder.withIndent('  ').convert(info));
 }
---
+++ tool/run_ci.dart
@@ -0,0 +1,12 @@
+import 'package:dev_test/package.dart';
+import 'package:process_run/shell.dart';
+Future<void> main() async {
+  final shell = Shell();
+
+  await shell.run('''
+# for info
+pub run chrome_travis:show_env
+''');
+  await packageRunCi('.');
+
+}
